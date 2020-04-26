d0a155ad4a572c23e7de18230552626e8a7249e4
Chris <devmhero@monthero.com>
2020-04-26 09:28:44 +0200

    nowy master po mergu i zmianach

--- GitTest/GameViewController.swift
+++ GitTest/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //komentarz
+        //komentarz3
         //komentarz numer 2
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'

a0e73d19fd7933e21941ce3e2ec21d4572eecdcd
Chris <devmhero@monthero.com>
2020-04-26 09:25:02 +0200

    komentarz numer 2

--- GitTest/GameViewController.swift
+++ GitTest/GameViewController.swift
@@ -14,7 +14,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //komentarz 
+        //komentarz
+        //komentarz numer 2
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
